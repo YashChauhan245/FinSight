811ea89f42120a3548bd5049f8eda7293b59b590
YashChauhan245 <yashchau.work@gmail.com>
2026-02-10 19:15:53 +0530

    feat(transactions): add account-chart

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -3,6 +3,7 @@ import { getAccountWithTransactions } from "@/actions/account";
 import { BarLoader } from "react-spinners";
 import { TransactionTable } from "../_components/transaction-table";
 import { notFound } from "next/navigation";
+import { AccountChart } from "../_components/account-chart";
 
 export default async function AccountPage({ params }) {
   const accountData = await getAccountWithTransactions(params.id);
@@ -35,6 +36,13 @@ export default async function AccountPage({ params }) {
           </p>
         </div>
       </div>
+
+      {/* Chart Section */}
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+      >
+        <AccountChart transactions={transactions} />
+      </Suspense>
 
       {/* Transactions Table */}
       <Suspense
